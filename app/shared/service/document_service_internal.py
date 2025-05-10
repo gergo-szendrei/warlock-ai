f9626ce1f19b5bb7c90533b0192229128e0bb3fd
Gergo Szendrei <gergo.szendrei@intuitech.studio>
2025-05-10 13:20:39 +0200

    Fix missing base_url for get_vector_store OllamaEmbeddings

--- app/shared/service/document_service_internal.py
+++ app/shared/service/document_service_internal.py
@@ -8,7 +8,11 @@ from langchain_postgres import PGVector
 def get_vector_store() -> PGVector:
     logging.debug(f"Calling get_vector_store")
 
-    embeddings: OllamaEmbeddings = OllamaEmbeddings(model=os.environ["LLM_MODEL"])
+    embeddings: OllamaEmbeddings = OllamaEmbeddings(
+        model=os.environ["LLM_MODEL"],
+        base_url=os.environ["LLM_BASE_URL"],
+        temperature=float(os.environ["LLM_TEMPERATURE"])
+    )
 
     user_info: str = f"{os.environ["POSTGRES_USER"]}:{os.environ["POSTGRES_PASSWORD"]}"
     db_info: str = f"{os.environ["POSTGRES_HOST"]}:{os.environ["POSTGRES_PORT"]}/{os.environ["POSTGRES_DATABASE"]}"
