fce548336b29e0d115b3e99e0b4ec5dc33576784
Gergo Szendrei <gergo.szendrei@intuitech.studio>
2025-06-02 14:17:13 +0200

    Add Angular CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.document_ingestion import document_ingestion_api
 from app.document_qa import document_qa_api
@@ -6,6 +7,14 @@ from app.general_qa import general_qa_api
 from app.health import health_api
 
 application = FastAPI()
+
+application.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 application.include_router(document_ingestion_api.router)
 application.include_router(document_qa_api.router)
