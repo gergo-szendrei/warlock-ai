b728d8d92dc3db3428d1cfe9267e0b2226c653e5
Gergo Szendrei <gergo.szendrei@intuitech.studio>
2025-06-13 16:04:45 +0200

    Turn off duck functionality until rate limit issue is not resolved

--- app/general_qa/agent/search_agent.py
+++ app/general_qa/agent/search_agent.py
@@ -7,7 +7,6 @@ from langchain.agents import (
     create_react_agent,
     AgentExecutor
 )
-from langchain_community.tools import DuckDuckGoSearchResults
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_ollama import ChatOllama
 
@@ -28,15 +27,24 @@ async def invoke_search_agent(
         disable_streaming=False
     )
 
+    # TODO - Switch back if Rate Limit issue has been solved
+    # instructions = """
+    #    You are an agent designed to utilize internet search engines to answer questions.
+    #    You have access to the capabilities of DuckDuckGo search.
+    #    Use the result of the search to answer the question.
+    #    If it does not seem like you can answer the question, just return "I don't know" as the answer.
+    # """
     instructions = """
-        You are an agent designed to utilize internet search engines to answer questions.
-        You have access to the capabilities of DuckDuckGo search.
-        Use the result of the search to answer the question.
+        You are an agent designed to utilize general knowledge to answer questions.
+        Use the result of the thought process to answer the question.
         If it does not seem like you can answer the question, just return "I don't know" as the answer.
     """
 
-    tools = [DuckDuckGoSearchResults()]
-    tool_names = ["duckduckgo_results_json"]
+    # TODO - Switch back if Rate Limit issue has been solved
+    # tools = [DuckDuckGoSearchResults()]
+    # tool_names = ["duckduckgo_results_json"]
+    tools = []
+    tool_names = []
 
     base_prompt = hub.pull(os.environ["LLM_REACT_PROMPT"])
     prompt = base_prompt.partial(
